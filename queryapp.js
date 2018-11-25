5233b6eb080f957cac5fcab5a62709b48da6de55
Paul O'M <mahoney@uk.ibm.com>
2018-11-25 16:21:37 +0000

    Update queryapp with function info
    Signed-off-by: Paul O'M <mahoney@uk.ibm.com>

--- queryapp.js
+++ queryapp.js
@@ -27,95 +27,83 @@ const wallet = new FileSystemWallet('../identity/user/isabella/wallet');
 // Main program function
 async function main() {
 
-  // A gateway defines the peers used to access Fabric networks
-  const gateway = new Gateway();
+    // A gateway defines the peers used to access Fabric networks
+    const gateway = new Gateway();
 
-  // Main try/catch block
-  try {
+    // Main try/catch block
+    try {
 
-    // Specify userName for network access
-    // const userName = 'isabella.issuer@magnetocorp.com';
-    const userName = 'User1@org1.example.com';
+        // Specify userName for network access
+        // const userName = 'isabella.issuer@magnetocorp.com';
+        const userName = 'User1@org1.example.com';
 
-    // Load connection profile; will be used to locate a gateway
-    let connectionProfile = yaml.safeLoad(fs.readFileSync('../gateway/networkConnection.yaml', 'utf8'));
+        // Load connection profile; will be used to locate a gateway
+        let connectionProfile = yaml.safeLoad(fs.readFileSync('../gateway/networkConnection.yaml', 'utf8'));
 
-    // Set connection options; identity and wallet
-    let connectionOptions = {
-      identity: userName,
-      wallet: wallet,
-      discovery: { enabled:false, asLocalhost: true }
-    };
+        // Set connection options; identity and wallet
+        let connectionOptions = {
+            identity: userName,
+            wallet: wallet,
+            discovery: { enabled:false, asLocalhost: true }
+        };
 
-    // Connect to gateway using application specified parameters
-    console.log('Connect to Fabric gateway.');
+        // Connect to gateway using application specified parameters
+        console.log('Connect to Fabric gateway.');
 
-    await gateway.connect(connectionProfile, connectionOptions);
+        await gateway.connect(connectionProfile, connectionOptions);
 
-    // Access PaperNet network
-    console.log('Use network channel: mychannel.');
+        // Access PaperNet network
+        console.log('Use network channel: mychannel.');
 
-    const network = await gateway.getNetwork('mychannel');
+        const network = await gateway.getNetwork('mychannel');
 
-    // Get addressability to commercial paper contract
-    console.log('Use org.papernet.commercialpaper smart contract.');
+        // Get addressability to commercial paper contract
+        console.log('Use org.papernet.commercialpaper smart contract.');
 
-    const contract = await network.getContract('papercontract', 'org.papernet.commercialpaper');
+        const contract = await network.getContract('papercontract', 'org.papernet.commercialpaper');
 
-    // issue commercial paper
-    console.log('Submit commercial paper issue transaction.');
+        /*
+        // QUERY the history of a commercial paper providing it the Issuer/paper number combo below
+        const queryResponse = await contract.submitTransaction('queryHist', 'MagnetoCorp', '00001');
+        let queryresult = CommercialPaper.fromBuffer(queryResponse);
 
-    const issueResponse = await contract.submitTransaction('issue', 'MagnetoCorp', '00001', '2020-05-31', '2020-11-30', '5000000');
+        console.log('the query result is ' + queryresult);
 
-    // process response
-    console.log('Process issue transaction response.');
+        console.log(`${queryresult.issuer} commercial paper : ${queryresult.paperNumber} successfully queried`);
+        console.log('Transaction complete.');
 
-    let paper = CommercialPaper.fromBuffer(issueResponse);
+       */
+        // query the OWNER of a commercial paper
+        const queryResponse2 = await contract.submitTransaction('queryOwner', 'MagnetoCorp', '00001');
+        let queryowneresult = CommercialPaper.fromBuffer(queryResponse2);
 
-    console.log(`${paper.issuer} commercial paper : ${paper.paperNumber} successfully issued for value ${paper.faceValue}`);
-    console.log('Transaction complete.');
+        console.log('the query result is ' + queryowneresult);
 
-// QUERY the history of a commercial paper providing it the Issuer/paper number combo below
-   const queryResponse = await contract.submitTransaction('queryHist', 'MagnetoCorp', '00001');
-   let queryresult = CommercialPaper.fromBuffer(queryResponse);
+        console.log(`${queryowneresult.issuer} commercial paper : ${queryowneresult.paperNumber} successfully queried`);
+        console.log('Transaction complete.');
 
-   console.log("the query result is " + queryresult);
-   
-   console.log(`${queryresult.issuer} commercial paper : ${queryresult.paperNumber} successfully queried`);
-   console.log('Transaction complete.');
+    } catch (error) {
 
-   // query the OWNER of a commercial paper
-   const queryResponse2 = await contract.submitTransaction('queryOwner', 'MagnetoCorp', '00045');
-   let queryowneresult = CommercialPaper.fromBuffer(queryResponse2);
+        console.log(`Error processing transaction. ${error}`);
+        console.log(error.stack);
 
-   console.log("the query result is " + queryowneresult);
+    } finally {
 
-   console.log(`${queryowneresult.issuer} commercial paper : ${queryowneresult.paperNumber} successfully queried`);
-   console.log('Transaction complete.');
-   
-  } catch (error) {
+        // Disconnect from the gateway
+        console.log('Disconnect from Fabric gateway.');
+        gateway.disconnect();
 
-    console.log(`Error processing transaction. ${error}`);
-    console.log(error.stack);
-
-  } finally {
-
-    // Disconnect from the gateway
-    console.log('Disconnect from Fabric gateway.')
-    gateway.disconnect();
-
-  }
+    }
 }
 main().then(() => {
 
-  console.log('Issue program complete.');
+    console.log('Issue program complete.');
 
 }).catch((e) => {
 
-  console.log('Issue program exception.');
-  console.log(e);
-  console.log(e.stack);
-  process.exit(-1);
+    console.log('Issue program exception.');
+    console.log(e);
+    console.log(e.stack);
+    process.exit(-1);
 
 });
-
